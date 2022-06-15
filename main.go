47b4f45d06abb34e4ee61367bdb728a17986eebe
Noa <boxship.noah@gmail.com>
2022-06-15 23:08:28 +0900

    add: (test)hello  world

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "log"
+    "github.com/hajimehoshi/ebiten"
+    "github.com/hajimehoshi/ebiten/ebitenutil"
+
+)
+
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+    return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+    ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return 320, 240
+}
+
+func main() {
+    ebiten.SetWindowSize(640, 480)
+    ebiten.SetWindowTitle("Hello, World!")
+    if err := ebiten.RunGame(&Game{}); err != nil {
+        log.Fatal(err)
+    }
+}
