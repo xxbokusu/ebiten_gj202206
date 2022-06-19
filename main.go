8625650e1bc7b52036a2ef72f5463857e32273a4
Noa <boxship.noah@gmail.com>
2022-06-19 20:39:55 +0900

    update:設計情報追加

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "log"
-    "github.com/hajimehoshi/ebiten"
-    "github.com/hajimehoshi/ebiten/ebitenutil"
+	"log"
 
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 type Game struct{}
 
 func (g *Game) Update(screen *ebiten.Image) error {
-    return nil
+	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-    ebitenutil.DebugPrint(screen, "Hello, World!")
+	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return 320, 240
+	return 320, 240
 }
 
 func main() {
-    ebiten.SetWindowSize(640, 480)
-    ebiten.SetWindowTitle("Hello, World!")
-    if err := ebiten.RunGame(&Game{}); err != nil {
-        log.Fatal(err)
-    }
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
