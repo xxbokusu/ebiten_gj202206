0beada1375cf98c39f81917b840f593dabd0747e
Noa <boxship.noah@gmail.com>
2022-06-25 09:22:48 +0900

    update:react for key press

--- main.go
+++ main.go
@@ -1,30 +1,82 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct{}
+const (
+	debug   = false
+	screenX = 800
+	screenY = 600
+
+	boardX = 9
+	boardY = 9
+)
+
+type board_panel struct {
+	state int
+}
+
+type Game struct {
+	board [boardX][boardY]board_panel
+}
+
+func (g *Game) isKeyJustPressed() bool {
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.board[1][1].state = 1
+		return true
+	}
+	return false
+}
 
 func (g *Game) Update() error {
+	if g.isKeyJustPressed() {
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.Black)
+	for i := 0; i < boardX; i = i + 1 {
+		for j := 0; j < boardY; j = j + 1 {
+			if g.board[i][j].state == 0 {
+				ebitenutil.DebugPrintAt(screen, "+", i*10, j*10)
+			} else {
+				ebitenutil.DebugPrintAt(screen, "O", i*10, j*10)
+			}
+		}
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 320, 240
 }
 
+func (g *Game) init() {
+	// board initialize
+	for i := 0; i < boardX; i = i + 1 {
+		for j := 0; j < boardY; j = j + 1 {
+			g.board[i][j].state = 0
+		}
+	}
+}
+
+// NewGame method
+func NewGame() *Game {
+	g := &Game{}
+	g.init()
+	return g
+}
+
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(screenX, screenY)
+	ebiten.SetWindowTitle("Magnet Go!")
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
